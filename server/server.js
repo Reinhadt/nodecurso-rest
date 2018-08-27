74bd6dbd6ef14d6990cd42d5bdcf57a0d73ff72d
Reinhadt <omavaloss@gmail.com>
2018-08-27 06:21:11 +0100

    Ahora con su JWT todo beio otra vez

--- server/server.js
+++ server/server.js
@@ -11,6 +11,10 @@ app.use(bodyParser.json())
 
 app.use(require('./routes/index'))
 
+mongoose.connect(process.env.URLDB, (err, res) =>{
+    if(err) throw err;
+    console.log('ROCANROLDB')
+});
 
 app.listen(process.env.PORT, () => {
     console.log("ROCANROL")
